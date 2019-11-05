addc79e1ef1fa65b0e5509497b92899e9baac948
kagendro <45953587+kagendro@users.noreply.github.com>
2019-11-04 21:44:09 -0500

    Delete 20191101235500_add_password_hash.exs

--- priv/repo/migrations/20191101235500_add_password_hash.exs
+++
@@ -1,9 +0,0 @@
-defmodule Time1.Repo.Migrations.AddPasswordHash do
-  use Ecto.Migration
-
-  def change do
-    alter table("workers") do
-      add :password_hash, :string, default: "", null: false
-    end
-  end
-end
