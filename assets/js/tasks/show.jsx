6ec620804b96b5ea64a41365c8104d97db7ece8a
Kevin Gendron <kevin.gendron182@gmail.com>
2019-11-06 22:20:19 -0500

    fixed show.jsx

--- assets/js/tasks/show.jsx
+++ assets/js/tasks/show.jsx
@@ -1,12 +1,36 @@
-import { get_photo } from '../ajax';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { connect } from 'react-redux';
+
+import { get_task } from '../ajax';
 
 function state2props(state, props) {
   let id = parseInt(props.id);
-  return {id: id, photo: state.photos.get(id)};
+  return {id: id, task: state.tasks.get(id)};
 }
 
-function PhotosShow({id, photo}) {
+function tasksShow({id, task}) {
   let image = null;
 
-  if (!photo) {
-    get_photo(id);
+  if (!task) {
+    get_task(id);
+
+    return (
+      <div>
+        <h1>Show task</h1>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Show Task</h1>
+      <img src={task.data} />
+      <p>Desc: {task.desc}</p>
+    </div>
+  );
+}
+
+export default connect(state2props)(tasksShow);
